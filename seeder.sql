df31f663b02758ea69c61e3d4b3757a0926e4f34
Hlib Babii <hlib.babii@unibz.it>
2017-11-17 12:14:26 +0100

    add script for booking table creation

---
+++ seeder.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS BOOKING (
+   ID             VARCHAR(50)                 NOT NULL,
+   ROOM_ID        VARCHAR(50)                 NOT NULL,
+   DAY            INT                         NOT NULL,
+   MONTH          INT                         NOT NULL,
+   YEAR           INT                         NOT NULL,
+   FROM_TIME      INT                         NOT NULL,
+   TO_TIME        INT                         NOT NULL,
+   TITLE          VARCHAR(200)                NOT NULL,
+   USER_ID        VARCHAR(50)                 NOT NULL,
+   PRIMARY KEY (ID)
+);
