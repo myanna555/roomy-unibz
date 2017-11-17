653904e0f3d228e1eaaf24d8fcc1042a723158ec
Hlib Babii <hlib.babii@unibz.it>
2017-11-17 02:31:35 +0100

    move spring config to a separate file

--- src/main/java/unibz/roomie/Main.java
+++ src/main/java/unibz/roomie/Main.java
@@ -16,19 +16,14 @@
 
 package unibz.roomie;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Map;
@@ -37,9 +32,6 @@ import java.util.Map;
 @Controller
 @SpringBootApplication
 public class Main {
-
-  @Value("${spring.datasource.url}")
-  private String dbUrl;
 
   @Autowired
   private DataSource dataSource;
@@ -68,17 +60,6 @@ public class Main {
     } catch (Exception e) {
       model.put("message", e.getMessage());
       return "error";
-    }
-  }
-
-  @Bean
-  public DataSource dataSource() throws SQLException {
-    if (dbUrl == null || dbUrl.isEmpty()) {
-      return new HikariDataSource();
-    } else {
-      HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(dbUrl);
-      return new HikariDataSource(config);
     }
   }
 
---
+++ src/main/java/unibz/roomie/SpringConfig.java
@@ -0,0 +1,31 @@
+package unibz.roomie;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+/**
+ * Created by hlib on 17.11.17.
+ */
+@Configuration
+public class SpringConfig {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() throws SQLException {
+        if (dbUrl == null || dbUrl.isEmpty()) {
+            return new HikariDataSource();
+        } else {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(dbUrl);
+            return new HikariDataSource(config);
+        }
+    }
+}
