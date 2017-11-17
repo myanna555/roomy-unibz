898206c16e3fff85ee3e7d528d81d017120f6b20
Hlib Babii <hlib.babii@unibz.it>
2017-11-17 13:20:55 +0100

    implement booking

--- src/main/java/unibz/roomie/controller/RoomController.java
+++ src/main/java/unibz/roomie/controller/RoomController.java
@@ -1,21 +1,31 @@
 package unibz.roomie.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import unibz.roomie.dao.DataService;
+import unibz.roomie.model.Booking;
 
 
 @RestController
 public class RoomController {
 
+    @Autowired
+    DataService dataService;
 
 	//to do
 
 	@RequestMapping(value = "/api/room/book", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
-	  public String bookARoom(@RequestBody String jsonString) {
-	  return jsonString;
+	  public String bookARoom(@RequestBody Booking booking) {
+        try {
+            dataService.book(booking);
+            return "OK";
+        } catch (DataService.BookingException e) {
+            return String.format("%s, Reason: %s", e.getMessage(), e.getCause().getMessage());
+        }
 	}
 
 
--- src/main/java/unibz/roomie/dao/DataService.java
+++ src/main/java/unibz/roomie/dao/DataService.java
@@ -2,12 +2,12 @@ package unibz.roomie.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import unibz.roomie.model.Booking;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * Created by hlib on 17.11.17.
@@ -15,24 +15,35 @@ import java.util.ArrayList;
 @Service
 public class DataService {
 
+    private final static String BOOK_QUERY =
+            "INSERT INTO BOOKING " +
+                    "(ROOM_ID, DAY, MONTH, YEAR, FROM_TIME, TO_TIME, TITLE, USER_ID) " +
+                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
     @Autowired
     private DataSource dataSource;
 
-    public String getSampleDataFromDb() {
+    public void book(Booking booking) throws BookingException {
         try (Connection connection = dataSource.getConnection()) {
-            Statement stmt = connection.createStatement();
-            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-            stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-            ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+            PreparedStatement stmt = connection.prepareStatement(BOOK_QUERY);
+            stmt.setString(1, booking.getRoomId());
+            stmt.setInt(2, booking.getDay());
+            stmt.setInt(3, booking.getMonth());
+            stmt.setInt(4, booking.getYear());
+            stmt.setInt(5, booking.getFromTime());
+            stmt.setInt(6, booking.getToTime());
+            stmt.setString(7, booking.getTitle());
+            stmt.setString(8, booking.getUserId());
 
-            ArrayList<String> output = new ArrayList<String>();
-            while (rs.next()) {
-                output.add("Read from DB: " + rs.getTimestamp("tick"));
-            }
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new BookingException("Room booking failed", e);
+        }
+    }
 
-            return output.toString();
-        } catch (Exception e) {
-            return e.getMessage();
+    public class BookingException extends Throwable {
+        public BookingException(String s, SQLException e) {
+            super(s, e);
         }
     }
 }
