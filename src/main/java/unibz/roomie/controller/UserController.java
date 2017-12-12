9688795cc5de3a99a5becf311ba44d2403cfd651
Hlib Babii <hlib.babii@unibz.it>
2017-12-12 16:31:16 +0100

    return 404 in http status rather than in the body for getUser request

--- src/main/java/unibz/roomie/controller/UserController.java
+++ src/main/java/unibz/roomie/controller/UserController.java
@@ -6,10 +6,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import unibz.roomie.dao.DataService;
@@ -95,26 +97,20 @@ public class UserController {
 		
 		//get user info by id
 		@RequestMapping(value = "/api/user/userinfo/{id}", method = RequestMethod.GET, produces = "application/json")
-		  public String getUserInfo(@PathVariable("id") int userId, HttpSession session) {		
-			
-	    try {	    	
-	    		if(userService.doesUserExist(new User(userId))) {
-	    			User userInfo = userService.getUserInfo(userId);
-	        		ObjectWriter writer = new ObjectMapper().writer().withDefaultPrettyPrinter();
-	        		String jsonUser = writer.writeValueAsString(userInfo);        		
-	        		String jsonCode = "{\"code\":\"200\",";
-	        		return jsonCode.concat(jsonUser.replaceAll("\\{", ""));
-	        }
-	        else
-	      	  return ERROR;
-	    } catch (JsonProcessingException e) {
-	  	  	return String.format("%s, Reason: %s", e.getMessage(), e.getCause().getMessage());
-	    		}
-	    catch (SQLException e) {
-	  	  	return String.format("%s, Reason: %s", e.getMessage(), e.getCause().getMessage());
-	    		}
-		}
-		
-	
-	
+		  public User getUserInfo(@PathVariable("id") int userId, HttpSession session) throws SQLException {
+
+            if (userService.doesUserExist(new User(userId))) {
+                return userService.getUserInfo(userId);
+            } else {
+                throw new UserNotExistsException("User " + userId + " does not exist", null);
+            }
+        }
+
+
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public static class UserNotExistsException extends RuntimeException {
+        public UserNotExistsException(String s, Exception e) {
+            super(s, e);
+        }
+    }
 }
